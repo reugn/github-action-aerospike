9a53e554ddd3161b21fdea1910a32daf6ec81e06
reugn <reugpro@gmail.com>
2021-04-13 16:21:16 +0300

    support Enterprise Edition setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 set -e
 
-if [ -n "$2" ]; then
-  docker_cmd="docker run -d --name gha_aerospike -p $1:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 aerospike --config-file /github/workspace/$2"
-else
-  docker_cmd="docker run -d --name gha_aerospike -p $1:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 aerospike"
+mount=""
+if [ -n "$3" ] || [ -n "$4" ]; then
+  config_dir="/github/workspace/$(dirname $3)"
+  mount="-v $config_dir:/opt/aerospike/etc"
 fi
+
+if [ -n "$4" ]; then
+  feature_key_file=$(basename $4)
+  image="-e \"FEATURE_KEY_FILE=/opt/aerospike/etc/$feature_key_file\" aerospike/aerospike-server-enterprise:$2"
+else
+  image="aerospike/aerospike-server:$2"
+fi
+
+if [ -n "$3" ]; then
+  config_file=$(basename $3)
+  docker_cmd="docker run -d --name gha_aerospike -p $1:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 \
+  $mount --config-file /opt/aerospike/etc/$config_file $image"
+else
+  docker_cmd="docker run -d --name gha_aerospike -p $1:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 $image"
+fi
+
+echo $docker_cmd
 
 sh -c "$docker_cmd"
