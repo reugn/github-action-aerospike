32f7cdd40b7e71212f04f97608c404651a854c1d
reugn <reugpro@gmail.com>
2021-07-20 10:50:41 +0300

    fix order of the custom configuration file parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ fi
 if [ -n "$3" ]; then
   config_file=$(basename $3)
   docker_cmd="docker run -d --name gha_aerospike -p $1:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 \
-  $mount --config-file /opt/aerospike/etc/$config_file $image"
+  $mount $image --config-file /opt/aerospike/etc/$config_file"
 else
   docker_cmd="docker run -d --name gha_aerospike -p $1:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 $image"
 fi
